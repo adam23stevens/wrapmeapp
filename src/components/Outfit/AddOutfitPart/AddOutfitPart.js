f3ebb954c844e3f2180389101405997a7f5aa398
Adam Stevens <adamstevens@Julias-MacBook-Air.local>
2017-12-18 08:10:04 +0000

    colour picker added

--- src/components/Outfit/AddOutfitPart/AddOutfitPart.js
+++ src/components/Outfit/AddOutfitPart/AddOutfitPart.js
@@ -14,11 +14,46 @@ const allAvailableItems = [
     'Trainers',
     'Boots'
 ]
+const allAvailableColours = [
+    'Default',
+    'Green',
+    'Red',
+    'Blue',
+    'Navy',
+    'Grey',
+    'BlackLeather',
+    'Tartan',
+    'Weave',
+    'DiagonalStripes',
+    'BlueDenim',
+    'BlackDenim'
+]
 
 class AddOutfitPart extends Component {
 
     state = {
-        displayIndex: 0
+        displayIndex: 0,
+        colourIndex: 0
+    }
+
+    prevColour = (prevState) => {
+        const prevIndex = (this.state.colourIndex -1 < 0) 
+                        ? allAvailableColours.length -1
+                        : this.state.colourIndex -1;
+
+        this.setState({
+            colourIndex: prevIndex
+        });
+    }
+
+    nextColour = (prevState) => {
+        const nextIndex = (this.state.colourIndex + 1 >= allAvailableColours.length)
+                        ? 0
+                        : this.state.colourIndex + 1;
+                
+        this.setState({
+            colourIndex: nextIndex
+        });
     }
 
     prevSelItem = (prevState) => {
@@ -44,21 +79,24 @@ class AddOutfitPart extends Component {
 
     render(){
         const selOutfitPart = allAvailableItems[this.state.displayIndex];
+        const selColour = allAvailableColours[this.state.colourIndex];
 
         return (
             <Wrap>
                 <h3>Add new outfit part</h3>
                 <p><i>{selOutfitPart}</i></p>
                 <div className={classes.outfitPartPickerWrap}>
-                    <Button clicked={this.prevSelItem} btnType="Success">Previous</Button>
+                    <button className={[classes.prev, classes.buttonLeft].join(' ')} onClick={this.prevSelItem}/>
+                    <button className={[classes.prevColour, classes.buttonLeft].join(' ')} onClick={this.prevColour}/>
                     <div className={classes.outfitPartPicker}>
                     <OutfitPartBuilder 
-                        colour='Default' 
+                        colour={selColour} 
                         type={selOutfitPart} 
                         style={{zIndex: '100'}}
                         display='add'/>
                     </div>
-                    <Button clicked={this.nextSelItem} btnType="Success">Next</Button>
+                    <button className={[classes.next, classes.buttonRight].join(' ')} onClick={this.nextSelItem}/>
+                    <button className={[classes.nextColour, classes.buttonRight].join(' ')} onClick={this.nextColour}/>
                 </div>
                 <Button clicked={this.props.cancelClicked} btnType='Danger'>Cancel</Button>
                 <Button clicked={this.props.continueClicked} btnType='Success'>Ok</Button>
